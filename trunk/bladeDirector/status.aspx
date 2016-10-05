04750a48a080f1f1c9e3cb83e08272d9c50f6b8d
Aliz <Aliz_hammond@d-crypt.com>
2016-10-05 14:39:36 +0800

    Add basic status page

---
+++ trunk/bladeDirector/status.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="status.aspx.cs" Inherits="bladeDirector.status" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Table ID="tblBladeStatus" runat="server"></asp:Table>
+    </div>
+    </form>
+</body>
+</html>
---
+++ trunk/bladeDirector/status.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace bladeDirector
+{
+    public partial class status : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            TableRow headerRow = new TableRow();
+            headerRow.Cells.Add(new TableCell() {Text = "State"});
+            headerRow.Cells.Add(new TableCell() {Text = "Blade IP"});
+            headerRow.Cells.Add(new TableCell() {Text = "Time since last keepalive"});
+            headerRow.Cells.Add(new TableCell() {Text = "Current owner"});
+            headerRow.Cells.Add(new TableCell() {Text = "Next owner"});
+
+            tblBladeStatus.Rows.Add(headerRow);
+
+            string[] bladeNames = hostStateDB.getAllBladeIP();
+
+            foreach (string bladeName in bladeNames)
+            {
+                bladeOwnership bladeInfo = hostStateDB.bladeStates.Single(x => x.bladeIP == bladeName);
+
+                TableRow newRow = new TableRow();
+                newRow.Cells.Add(new TableCell() {Text = bladeInfo.state.ToString()});
+                newRow.Cells.Add(new TableCell() {Text = bladeInfo.bladeIP});
+                newRow.Cells.Add(new TableCell() {Text = (DateTime.Now - bladeInfo.lastKeepAlive).ToString()});
+                newRow.Cells.Add(new TableCell() { Text = bladeInfo.currentOwner ?? "none" });
+                newRow.Cells.Add(new TableCell() {Text = bladeInfo.nextOwner ?? "none" });
+
+
+                tblBladeStatus.Rows.Add(newRow);
+            }
+        }
+    }
+}
