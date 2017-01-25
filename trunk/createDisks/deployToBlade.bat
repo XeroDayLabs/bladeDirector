3c85c18901e9e404c9ab21f326bf566189d86b01
Aliz <Aliz_hammond@d-crypt.com>
2017-01-25 14:46:06 +0800

    Add forgotten file

---
+++ trunk/createDisks/deployToBlade.bat
@@ -0,0 +1,27 @@
+@echo on
+REM usage : deployToBlade.bat <computername> <kernel debug server IP> <kernel debug server port>
+
+REM Name the computer according to our parameters
+wmic computersystem where caption='%COMPUTERNAME%' call rename %1
+
+REM enable the kernel debugger
+bcdedit /dbgsettings net hostip:%2 port:%3
+bcdedit /debug on
+
+REM drop down to a single CPU
+bcdedit /set NUMPROC 1
+
+REM Instruct WSUS to regen its SID so that it operates correctly
+reg delete \"HKLM\\Software\\Microsoft\\Windows\\CurrentVersion\\WindowsUpdate\" /v SusClientId /f
+reg delete \"HKLM\\Software\\Microsoft\\Windows\\CurrentVersion\\WindowsUpdate\" /v SusClientIdValidation /f
+reg add \"HKLM\\Software\\Microsoft\\Windows\\CurrentVersion\\WindowsUpdate\" /v TargetGroup /t REG_SZ /d "blades" /f
+reg add \"HKLM\\Software\\Microsoft\\Windows\\CurrentVersion\\WindowsUpdate\" /v TargetGroupEnabled /t REG_DWORD /d 1 /f
+net stop wuauserv
+net start wuauserv
+wuauclt.exe /resetauthorization /detectnow
+
+REM Now we must wait for WSUS to do its thing. How do we do that? MSDN says "wait 10 minutes for a detection cycle to finish". :(
+REM Let's just wait for one minute.
+timeout 60
+
+exit 0
