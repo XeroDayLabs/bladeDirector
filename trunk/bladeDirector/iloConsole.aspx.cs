16eb429055c67a9421c4cc74e949c372f684f282
aliz <aliz@xd.lan>
2017-11-21 17:56:18 +0800

    Add forgotten files

---
+++ trunk/bladeDirector/iloConsole.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using bladeDirectorClient;
+using hypervisors;
+
+namespace bladeDirector
+{
+    public partial class iloConsole : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Request.QueryString.HasKeys())
+                return;
+            string ip = Request.QueryString["bladeip"];
+
+            string serverURL = status.getCurrentServerURL(this);
+            if (serverURL == null)
+            {
+                Response.Write("you're not logged in");
+            }
+            else
+            {
+                using (BladeDirectorServices services = new BladeDirectorServices(serverURL))
+                {
+                    var spec = services.svc.getBladeByIP_withoutLocking(ip);
+
+                    using (
+                        hypervisor_iLo_HTTP theIlo = new hypervisor_iLo_HTTP(spec.iLOIP, spec.iLoUsername,
+                            spec.iLoPassword))
+                    {
+                        theIlo.logoutOnDisposal = false;
+                        theIlo.connect();
+                        Response.Redirect(theIlo.makeHPLOLink());
+                    }
+                }
+            }
+        }
+    }
+}
