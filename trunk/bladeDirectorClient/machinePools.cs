e1d8843f6d33cda96d623219567fa3617d228d93
aliz <aliz@xd.lan>
2018-04-03 16:33:53 +0800

    Allow machine pools to be destroyed (although not very cleanly), mostly so that tests can use different machine pools

--- trunk/bladeDirectorClient/machinePools.cs
+++ trunk/bladeDirectorClient/machinePools.cs
@@ -4,7 +4,14 @@
     {
         public static string bladeDirectorURL = Properties.Settings.Default.bladeDirectorURL;
 
-        public static readonly iLoHypervisorPool ilo = new iLoHypervisorPool();
-        public static readonly VMWareHypervisorPool vmware = new VMWareHypervisorPool();
+        public static iLoHypervisorPool ilo = new iLoHypervisorPool();
+        public static VMWareHypervisorPool vmware = new VMWareHypervisorPool();
+
+        public static void reinit()
+        {
+            bladeDirectorURL = Properties.Settings.Default.bladeDirectorURL;
+            ilo = new iLoHypervisorPool();
+            vmware = new VMWareHypervisorPool();
+        }
     }
 }
